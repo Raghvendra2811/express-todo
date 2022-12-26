53fb80975a4a846d8b0f18f906cde024a6988f9e
Raghvendra2811 <85832090+Raghvendra2811@users.noreply.github.com>
2022-12-26 21:37:15 +0530

    todo backend

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+
+// app.get("/", (req, res) => res.type('html').send());
+
+
+
+
+app.get("/todos", async (req, res) => {
+
+  const todos = await  getAllTodos();
+
+  return res.send({
+    data: todos,
+  });
+});
+
+
+
+app.post("/todos", (req, res) => {
+  const todosData = req.body;
+  const todo = addTodo(todosData);
+  return res.send({
+    data: todo,
+  });
+});
+
+
+
+app.patch("/todos/:id", (req, res) => {
+  const Data = req.body;
+  const id = req.params.id;
+
+const todo = updateTodoById(Number(id), Data);
+
+return res.send({
+  data: todo,
+});
+});
+
+
+
+
+app.delete("/todos/:id", (req, res) => {
+  const id = req.params.id;
+  const todo = deleteTodo(Number(id));
+  if(todo){
+      return res.send({
+          data: todo,
+        });
+    }else{
+      return res.status(404).send({
+          message: "Todo does not exist"
+      })
+    }
+
+});
+
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
---
+++ todos.js
@@ -0,0 +1,70 @@
+const e = require("express");
+const fs = require("fs/promises");
+
+const todos = [
+  {
+    id: 1,
+    taskname: "learn node",
+    status :false,
+    tag:"personal"
+  }
+];
+
+async function getAllTodos() {
+    let data = JSON.stringify(todos, null, 2);
+    // console.log(data);
+    return todos;
+  }
+
+
+  function addTodo(todo) {
+    console.log(todo);
+    let max = 0;
+    todos.forEach((ele) => {
+      if (max < ele.id) {
+        max = ele.id;}});
+    let newId = max + 1;
+    const newTodoData = {
+      ...todo,
+      id: newId,
+    };
+    todos.push(newTodoData);
+    return newTodoData;
+  }
+
+  function deleteTodo(id) {
+    let index = -1;
+  
+    todos.forEach((ele, i) => {
+      if (ele.id == id) {
+        index = i;}});
+    let tododelete;
+    if (index != -1) {
+      let result = todos.splice(index, 1);
+  
+      if (result.length) {
+        tododelete = result[0];
+      }
+    }
+    return tododelete;
+  }
+
+
+  function updateTodoById(id, todoData) {
+    let index = -1;
+  
+    todos.forEach((ele, i) => {
+      if (ele.id == id) {
+        index = i;
+      }
+    });
+  
+    if (index != -1) {
+      todos[index] = {
+        ...todos[index],
+        ...todoData,
+      };
+    }
+    return todos[index];
+  }
+  
