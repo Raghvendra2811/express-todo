af09ab1b3d61732cf896d46fcd2714b163642614
Raghvendra2811 <85832090+Raghvendra2811@users.noreply.github.com>
2022-12-28 16:19:18 +0530

    commit

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
+app.use(express.json())
+app.use(cors())
+
 
 app.use(bodyParser.json());
 const port = process.env.PORT || 3001;
